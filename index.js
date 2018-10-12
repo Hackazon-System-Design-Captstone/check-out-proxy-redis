d980bdf66b11ef3a9c5cdaca14db735b8c685a4a
Andrew Ngo <andrew@Andrews-MacBook-Pro-3.local>
2018-10-12 13:52:17 -0700

    Includes Includes new relic

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('newrelic');
+
 const express = require('express');
 const redis = require("redis");
-
+const axios = require('axios');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-// let client = redis.createClient('ec2-52-53-232-39.us-west-1.compute.amazonaws.com:3005');
+// let client = redis.createClient();
 //
 // client.on('connect', function(error) {
 //   if (error) {
@@ -14,14 +17,32 @@ const app = express();
 //   }
 // });
 
+app.use(express.static('./client/dist'));
 
+let serverCluster = ['http://ec2-52-53-226-39.us-west-1.compute.amazonaws.com:3004/checkout/',
+  'http://ec2-54-153-49-80.us-west-1.compute.amazonaws.com:3004/checkout/',
+  'http://ec2-13-57-183-138.us-west-1.compute.amazonaws.com:3004/checkout/'
+]
+let count = 0;
 
 app.get('/checkout/:id', (req, res) => {
 
-  axios.get('load-balancer-2-864280534.us-west-1.elb.amazonaws.com/?=' + req.params.id)
-    .then(function(response) {
+  // client.get(req.params.id, (error, results) => {
+  //
+  //   if (results !== null) {
+  //     res.send(JSON.parse(results))
+  //   } else {
+  //
+  //     }
+  //   })
+
+console.log(serverCluster[count % 3])
+  axios.get(serverCluster[count % 3] + req.params.id)
+    .then(function({ data }) {
       // client.setex(req.params.id, 1000000, JSON.stringify(response.data));
-      res.send(response.data);
+      console.log(data)
+      count++
+      res.send(data);
     })
     .catch(function(error) {
       res.send(error);
