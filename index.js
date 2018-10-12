11c3bb389dfd4aec1dc5ea6ff5cb66b96daf8c6c
Andrew Ngo <andrew@Andrews-MacBook-Pro-3.local>
2018-10-12 14:19:03 -0700

    Includes loader token

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ app.get('/checkout/:id', (req, res) => {
   // })
 })
 
-app.get('/loaderio-4abc64da50cda623147ca41bd3f94574.txt', (req, res) => {
-  res.send('loaderio-4abc64da50cda623147ca41bd3f94574')
+app.get('/loaderio-f1add894f148e948cc65b45c3070f581.txt', (req, res) => {
+  res.send('loaderio-f1add894f148e948cc65b45c3070f581')
 })
 
 app.listen(3005, () => console.log('Listening on port 3005...'));
